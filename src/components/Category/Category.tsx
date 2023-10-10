5f7950f780ee7fdd7dbf82ce260d391fe8e741b3
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-10 21:29:37 +0300

    category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
+import './Category.css';
+
 const category = [
     {name: "Заметки", id: 0},
     {name: "Создать план на день", id: 1},
-]
+];
 
 export const Category = () => {
   return (
     <div className='w-screen'>
         <ul className='flex justify-center gap-4'>
             {category.map((item, index) => (
-                <li className='button p-1 pl-3 pr-3 text-sm rounded-md' key={index}>{item.name}</li>
+                <li className='category p-1 pl-3 pr-3 text-sm rounded-md cursor-pointer' key={index}>{item.name}</li>
             ))}
         </ul>
     </div>
