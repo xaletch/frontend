b98ea8c8462c29874e3973b76c191f3292e02b1e
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-10 22:48:57 +0300

    loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -7,7 +7,7 @@ export const Loading = () => {
 
     return (
         <div className='h-screen flex pt-32 justify-center'>
-            <h1 className='p-2 text-sm text-center'><span>{user?.first_name}</span>, добро пожаловать!</h1>
+            <h1 className='p-2 text-sm text-center'><span className='capitalize'>{user?.first_name}</span>, добро пожаловать!</h1>
         </div>
     )
 }
