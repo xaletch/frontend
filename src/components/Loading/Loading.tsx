3b8c2fddbf1090f7581a0d26560310138d1e6903
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-10 22:30:51 +0300

    new

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -6,7 +6,7 @@ export const Loading = () => {
     const { user } = useTelegram();
 
     return (
-        <div className='h-screen flex items-center justify-center'>
+        <div className='h-screen flex pt-32 justify-center'>
             <h1 className='p-2 text-sm text-center'><span>{user?.first_name}</span>, добро пожаловать!</h1>
         </div>
     )
