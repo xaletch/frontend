3e596b0d4f9f4fdfc54979970ff172ec1350ae5a
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-10 23:06:18 +0300

    category

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -9,7 +9,8 @@ const category = [
 
 export const Category = () => {
   return (
-    <div className='w-screen p-4 flex items-center justify-center'>
+    <div className='w-screen flex items-center justify-center flex-col'>
+        <p className='mb-20 text-sm text-center'>Здесь вы можете управлять своими заметками. <br/>  Но для начала вам предстоит создать забетку/план.</p>
         <ul>
             {category.map((item, index) => (
                 <li className='category p-4 pl-6 pr-6 text-sm mb-5 text-center rounded-md cursor-pointer' key={index}>{item.name}</li>
