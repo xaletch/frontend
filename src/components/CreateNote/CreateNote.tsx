e676119d8502e285cbafc05e381dfa26e68f1006
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-11 13:41:31 +0300

    note input style

--- src/components/CreateNote/CreateNote.tsx
+++ src/components/CreateNote/CreateNote.tsx
@@ -55,15 +55,15 @@ export const CreateNote: React.FC = () => {
           <input className='input w-screen p-4 pl-6 pr-14 text-sm outline-none' type='text' ref={inputRef} value={inputValue} onChange={handleChangeInput} onKeyDown={handleKeyDown} placeholder='обновить бота'/>
           {inputValue && 
             <div className='absolute right-2 flex'>
-              <div className='w-8 h-10 flex justify-center items-center transition ease-in-ou rounded cursor-pointer  hover:bg-slate-200' onClick={handleNoteAdd}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#696969" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <div className='node-btn w-8 h-10 flex justify-center items-center transition ease-in-ou rounded cursor-pointer' onClick={handleNoteAdd}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#FFf" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                   <path d="M20 6L9 17l-5-5"></path>
                 </svg>
               </div>
-              <div onClick={handleNoteAdd} className='w-8 h-10 flex justify-center items-center transition ease-in-ou rounded cursor-pointer  hover:bg-slate-200'>
+              <div onClick={handleNoteAdd} className='node-btn w-8 h-10 flex justify-center items-center transition ease-in-ou rounded cursor-pointe'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 25 25" fill="none">
-                  <path d="M12.2473 4.94844C7.36828 4.94844 3.24728 9.06944 3.24728 13.9484C3.24728 18.8274 7.36828 22.9484 12.2473 22.9484C17.1263 22.9484 21.2473 18.8274 21.2473 13.9484C21.2473 9.06944 17.1263 4.94844 12.2473 4.94844ZM12.2473 20.9484C8.45328 20.9484 5.24728 17.7424 5.24728 13.9484C5.24728 10.1544 8.45328 6.94844 12.2473 6.94844C16.0413 6.94844 19.2473 10.1544 19.2473 13.9484C19.2473 17.7424 16.0413 20.9484 12.2473 20.9484Z" fill="#696969"/>
-                  <path d="M13.2473 12.9484V8.94844H11.2473V14.9484H17.2473V12.9484H13.2473ZM17.5313 4.65544L18.9433 3.23944L21.9533 6.23944L20.5403 7.65644L17.5313 4.65544ZM6.94528 4.65544L3.95528 7.65444L2.53728 6.24244L5.52728 3.24244L6.94528 4.65544Z" fill="#696969"/>
+                  <path d="M12.2473 4.94844C7.36828 4.94844 3.24728 9.06944 3.24728 13.9484C3.24728 18.8274 7.36828 22.9484 12.2473 22.9484C17.1263 22.9484 21.2473 18.8274 21.2473 13.9484C21.2473 9.06944 17.1263 4.94844 12.2473 4.94844ZM12.2473 20.9484C8.45328 20.9484 5.24728 17.7424 5.24728 13.9484C5.24728 10.1544 8.45328 6.94844 12.2473 6.94844C16.0413 6.94844 19.2473 10.1544 19.2473 13.9484C19.2473 17.7424 16.0413 20.9484 12.2473 20.9484Z" fill="#FFf"/>
+                  <path d="M13.2473 12.9484V8.94844H11.2473V14.9484H17.2473V12.9484H13.2473ZM17.5313 4.65544L18.9433 3.23944L21.9533 6.23944L20.5403 7.65644L17.5313 4.65544ZM6.94528 4.65544L3.95528 7.65444L2.53728 6.24244L5.52728 3.24244L6.94528 4.65544Z" fill="#FFf"/>
                 </svg>
               </div>
             </div>
