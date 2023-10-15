2cb97a2dd737278238364d18c5f8b94a3f7c8c6d
xaletch <kiril.kolesnikov0609@mail.ru>
2023-10-15 20:56:14 +0300

    new

--- src/components/NotePage/Menu/Menu.tsx
+++ src/components/NotePage/Menu/Menu.tsx
@@ -29,7 +29,7 @@ export const Menu: React.FC<MenuInterface> = ({ setMenuOpen }) => {
               </span>
               <span className='text-base font-medium capitalize'>{user?.first_name}</span>
             </div>
-            <div className='cursor-pointer flex items-center' onClick={handleCloseMenu}>
+            <div className='p-1 cursor-pointer flex items-center rounded hover:bg-grey' onClick={handleCloseMenu}>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 16 12" fill="none">
                 <path d="M0 0H16V2H0V0ZM4 5H16V7H4V5ZM9 10H16V12H9V10Z" fill="#8f8f8f" fillOpacity="0.8"/>
               </svg>
